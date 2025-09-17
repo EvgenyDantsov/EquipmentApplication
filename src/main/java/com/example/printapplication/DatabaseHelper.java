e8a595ecfe3cea0c9913f342d4a63f3338a16519
Evgeny Dantsov <adriano-sport@mail.ru>
2025-09-17 09:57:10 +0300

    Update database handling for equipment tracking and added method to fetch all records by equipment type

--- src/main/java/com/example/printapplication/DatabaseHelper.java
+++ src/main/java/com/example/printapplication/DatabaseHelper.java
@@ -37,19 +37,20 @@ public class DatabaseHelper {
                 "    Office.name_office, \n" +
                 "    Office.number_office, \n" +
                 "    Department.department_name, \n" +
-                "    Printer.printer_name, \n" +
-                "    Printer.model, \n" +
-                "    Printer.sn_number, \n" +
-                "    Printer.note, \n" +
-                "    Printer.status, \n" +
+                "    equipment.name, \n" +
+                "    equipment.model, \n" +
+                "    equipment.sn_number, \n" +
+                "    equipment.note, \n" +
+                "    equipment.status, \n" +
                 "    SeniorDepartment.fio,\n" +
-                "    Printer.id\n" +
+                "    equipment.id, \n" +
+                "    equipment.equipmenttype_id \n" +
                 "FROM \n" +
                 "    Office\n" +
                 "INNER JOIN \n" +
                 "    Department ON Office.Department_id = Department.id\n" +
                 "INNER JOIN \n" +
-                "    Printer ON Office.id = Printer.Office_id\n" +
+                "    equipment ON Office.id = equipment.Office_id\n" +
                 "INNER JOIN \n" +
                 "    SeniorDepartment ON Department.id = SeniorDepartment.Department_id";
         try (PreparedStatement statement = getConnection().prepareStatement(sql);
@@ -60,14 +61,62 @@ public class DatabaseHelper {
                         resultSet.getString("name_office"),
                         resultSet.getString("number_office"),
                         resultSet.getString("department_name"),
-                        resultSet.getString("printer_name"),
+                        resultSet.getString("name"),
                         resultSet.getString("model"),
                         resultSet.getString("sn_number"),
                         resultSet.getString("note"),
                         resultSet.getString("status"),
                         resultSet.getString("fio")
-                );record.setPrinterId(resultSet.getInt("id")); // Устанавливаем ID принтера
+                );
+                record.setEquipmentId(resultSet.getInt("id")); // Устанавливаем ID принтера
+                record.setEquipmentTypeId(resultSet.getInt("equipmenttype_id"));
                 records.add(record);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return records;
+    }
+    public static List<MainRecord> getAllViewByType(int equipmentTypeId) {
+        ObservableList<MainRecord> records = FXCollections.observableArrayList();
+        String sql =
+                "SELECT " +
+                        "  Office.name_office, " +
+                        "  Office.number_office, " +
+                        "  Department.department_name, " +
+                        "  equipment.name, " +
+                        "  equipment.model, " +
+                        "  equipment.sn_number, " +
+                        "  equipment.note, " +
+                        "  equipment.status, " +
+                        "  SeniorDepartment.fio, " +
+                        "  equipment.id, " +
+                        "  equipment.equipmenttype_id " +
+                        "FROM Office " +
+                        "INNER JOIN Department ON Office.Department_id = Department.id " +
+                        "INNER JOIN equipment  ON Office.id = equipment.Office_id " +
+                        "INNER JOIN SeniorDepartment ON Department.id = SeniorDepartment.Department_id " +
+                        "WHERE equipment.equipmentType_id = ?";
+
+        try (PreparedStatement st = getConnection().prepareStatement(sql)) {
+            st.setInt(1, equipmentTypeId);
+            try (ResultSet rs = st.executeQuery()) {
+                while (rs.next()) {
+                    MainRecord r = new MainRecord(
+                            rs.getString("name_office"),
+                            rs.getString("number_office"),
+                            rs.getString("department_name"),
+                            rs.getString("name"),
+                            rs.getString("model"),
+                            rs.getString("sn_number"),
+                            rs.getString("note"),
+                            rs.getString("status"),
+                            rs.getString("fio")
+                    );
+                    r.setEquipmentId(rs.getInt("id")); // лучше переименовать в setEquipmentId
+                    r.setEquipmentTypeId(rs.getInt("equipmentType_id"));
+                    records.add(r);
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
